0bd0b5f74026079c302322fbaaf5105dbdf20541
Vinicius Cerqueira <viniciusleitecerqueira@gmail.com>
2025-06-22 14:46:21 -0300

    adicionados metodos get, delete e login

--- src/main/java/com/viniciuscerqueira/usuario/controller/UsuarioController.java
+++ src/main/java/com/viniciuscerqueira/usuario/controller/UsuarioController.java
@@ -3,12 +3,15 @@ package com.viniciuscerqueira.usuario.controller;
 
 import com.viniciuscerqueira.usuario.business.UsuarioService;
 import com.viniciuscerqueira.usuario.business.dto.UsuarioDTO;
+import com.viniciuscerqueira.usuario.infrastructure.entity.Usuario;
+import com.viniciuscerqueira.usuario.infrastructure.exceptions.ConflictException;
+import com.viniciuscerqueira.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -16,11 +19,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO){
          return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
-
     }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(), usuarioDTO.getSenha())
+        );
+        return jwtUtil.generateToken(authentication.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscaUsuarioPorEmail(@RequestParam("email") String email){
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable String email){
+        usuarioService.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
+
 
 }
---
+++ src/main/java/com/viniciuscerqueira/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.viniciuscerqueira.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+
+
+}
